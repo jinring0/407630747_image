d8e551a1cef687cec9bda3360a01566b65a82a13
jinring0 <sexylikeagirl30678@gmail.com>
2022-11-16 14:47:29 +0800

    add pic

--- lib/main.dart
+++ lib/main.dart
@@ -30,30 +30,38 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
         title: Text('407630747_image'),
+    );
+
+    final widget = Center(
+        child: Stack(children: <Widget>[
+      Container(
+      child: imgBrowser,
     ),
-    body: Center(
-    // Center is a layout widget. It takes a single child and positions it
-    // in the middle of the parent.
-    child: Column(
-    // Column is also a layout widget. It takes a list of children and
-    // arranges them vertically. By default, it sizes itself to fit its
-    // children horizontally, and tries to be as tall as its parent.
-    //
-    // Invoke "debug painting" (press "p" in the console, choose the
-    // "Toggle Debug Paint" action from the Flutter Inspector in Android
-    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-    // to see the wireframe for each widget.
-    //
-    // Column has various properties to control how it sizes itself and
-    // how it positions its children. Here we use mainAxisAlignment to
-    // center the children vertically; the main axis here is the vertical
-    // axis because Columns are vertical (the cross axis would be
-    // horizontal).
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-
-
-
-  }
+      Container(
+      child: Row(
+        children: <Widget>[previousBtn, nextBtn],
+        mainAxisAlignment: MainAxisAlignment.centor: ,
+    ),
+    margin:  EdgeInsets.symmetric(vertical: 10),
+    ),
+    ],alignment: Alignment.topCenter),
+    );
 }
+}
+
+class _ImageBrowserState extends State<_ImageBrowser>{
+    override
+  widget build(BuildContext context){
+      var img = PhotoView(
+          imageProvider: AssetImage(widget,_images[widget._imageIndex])
+          minScale: PhotoViewComputedScale.contained * 0.6,
+          maxScale: PhotoViewComputedScale.covered,
+          enableRotation: true,
+          backgroundDecoration: BoxDecoration(
+            colors: Colors.white,
+  ));
+      return img;
+  }
+  }
+
 
